42d64ee21bf5279f51b008a3c678adcd20a2d184
Sabrina A <sabrina@Sabrinas-MBP.net>
2022-10-12 12:41:31 -0600

    updated components and pages folders

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,16 +1,17 @@
 import { Link } from "react-router-dom";
-import { AiFillGithub } from 'react-icons/ai';
-import { AiFillLinkedin } from 'react-icons/ai';
+import { AiFillGithub } from "react-icons/ai";
+import { AiFillLinkedin } from "react-icons/ai";
 import "./NavBar.css";
+import Logo from "../../images/Logo1.png";
 
 const NavBar = () => {
-    return (
-        <div>
-          <nav className="navbar custom navbar-expand-lg p-3" id="navbar">
-        <div className="container-fluid d-flex justify-content-center">
+  return (
+    <div>
+      <nav className="navbar custom navbar-expand-lg p-3" id="navbar">
+        <div className="container-fluid d-flex justify-content-end">
           <a href="/" className="navbar-brand col">
-            Logo
-            {/* <img id="company-logo" src={CompanyLogo} alt='Company Logo' /> */}
+          <img id="logo" src={Logo} alt='Logo' />
+
           </a>
           <button
             className="navbar-toggler"
@@ -25,43 +26,57 @@ const NavBar = () => {
             <span className="navbar-toggler-icon" id="navbar-toggler"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav ms-auto mb-0 mb-lg-0">
-          <li className="nav-item mt-1 underline">
-            <Link className="nav-link text-white" to="/about">
-                About
-            </Link>
-            </li>
-          <li className="nav-item mt-1 underline">
-            <Link className="nav-link" to="/projects">
-                Projects
-            </Link>
-            </li>
-          <li className="nav-item mt-1 underline">
-            <a className="nav-link" href="mailto:sabrinaangelle@ymail.com">
-                Contact
+            <ul className="navbar-nav ms-auto mb-0 mb-lg-0">
+              <li className="nav-item mt-1 underline">
+                <Link className="nav-link" to="/about">
+                  About
+                </Link>
+              </li>
+              <li className="nav-item mt-1 underline">
+                <Link className="nav-link" to="/projects">
+                  Projects
+                </Link>
+              </li>
+              <li className="nav-item mt-1 underline">
+                <a className="nav-link" href="mailto:sabrinaangelle@ymail.com">
+                  Contact
                 </a>
-            </li>
-          <li className="nav-item mt-1 underline">
-            <a className="nav-link" href="https://www.canva.com/design/DAFHwaDxw34/mZmh7VsekQK5w9byzgLrxw/view?utm_content=DAFHwaDxw34&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton" target="_blank">
-               Resume
-               </a>
-            </li>
-          <li className="nav-item mt-1"> 
-            <a className="nav-link" href="https://github.com/s-angelle" id="github-link" target="_blank">
-          <AiFillGithub id="github-icon"/>
-          </a>
-            </li>
-          <li className="nav-item mt-1">
-            <a className="nav-link" href="https://www.linkedin.com/in/sabrina-angelle"id="linkedin-link" target="_blank">
-               <AiFillLinkedin  id="linkedin-icon"/>
-               </a>
-            </li>
+              </li>
+              <li className="nav-item mt-1 underline">
+                <a
+                  className="nav-link"
+                  href="https://www.canva.com/design/DAFHwaDxw34/mZmh7VsekQK5w9byzgLrxw/view?utm_content=DAFHwaDxw34&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton"
+                  target="_blank"
+                >
+                  Resume
+                </a>
+              </li>
+              <li className="nav-item mt-1">
+                <a
+                  className="nav-link"
+                  href="https://github.com/s-angelle"
+                  id="github-link"
+                  target="_blank"
+                >
+                  <AiFillGithub id="github-icon" />
+                </a>
+              </li>
+              <li className="nav-item mt-1">
+                <a
+                  className="nav-link"
+                  href="https://www.linkedin.com/in/sabrina-angelle"
+                  id="linkedin-link"
+                  target="_blank"
+                >
+                  <AiFillLinkedin id="linkedin-icon" />
+                </a>
+              </li>
             </ul>
           </div>
         </div>
       </nav>
-        </div>
-    );
-}
+    </div>
+  );
+};
 
 export default NavBar;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import "./Home.css";
+import HomepagePhoto from "../../images/Futuristic-photo1.jpeg";
 
 const Home = () => {
     return (
-        <div id="heading-div">
+        <div id="greeting-div">
             <h5> Hi, my name is</h5>
-            <h1>Sabrina Angelle</h1>
-            <h3> I'm a web developer who thoroughly enjoys creating a visually stimulating, user-friendly experience. I'm currently seeking a collaborative environment to further enhance my coding skills. </h3>
+            <h1 id="author-name" >Sabrina Angelle</h1>
+            <h3 id="intro-div"> I'm a web developer who thoroughly enjoys creating a visually stimulating, user-friendly experience. I'm currently seeking a collaborative environment to further enhance my coding skills. </h3>
+            {/* <img id="homepage-photo" src={HomepagePhoto} /> */}
         </div>
     );
 }
